2f7273e31c81d32caded35308f80573761759a7c
Christian Otieno <cgao273@gmail.com>
2018-09-12 16:03:46 +0300

    Task 3 (Pattern Matching)

--- Task3.scala
+++ Task3.scala
@@ -1,4 +1,4 @@
-case class Person(name: String, age: Int)
+case class Person(firstName: String, lastName: Int)
 object Test2 extends App {
     def matchType(x: Any): String = x match {
       //case x: List(1, _*) => s"$x"          // doesn't compile
@@ -6,9 +6,9 @@ object Test2 extends App {
       //case Some(_) => "got a Some"          // works, but can't access the Some
       //case Some(x) => s"$x"                 // works, returns "foo"
       case x @ Some(_) => s"$x"               // works, returns "Some(foo)"
-      case p @ Person(name, 23) => s"$p"  // works, returns "Person(Christian,23)"
+      case p @ Person(first, "Otieno") => s"$p"  // works, returns "Person(Christian,Otieno)"
   }
   println(matchType(List(1,2,3)))             // prints "List(1, 2, 3)"
   println(matchType(Some("foo")))             // prints "Some(foo)"
-  println(matchType(Person("Christian", 23)))   // prints "Person(Christian,23)"
+  println(matchType(Person("Christian","Otieno")))   // prints "Person(Christian,Otieno)"
 }
